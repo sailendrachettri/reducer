6147e69de8f0cc243995f0e734856f4aa21222d3
sailendrachettri <sailendra9083@gmail.com>
2024-04-30 14:30:35 +0530

    footer link changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 export default function Footer() {
-    let githubLink = "https://github.com/sailendrachettri";
+    let footerLink = "https://sailendra.onrender.com";
 
     useEffect(() => {
         let footerYear = document.getElementById("year");
@@ -14,7 +14,7 @@ export default function Footer() {
     return (
         <footer>
             <div className="footer-section">
-                <div className="">Developed with ❤ by <a href={githubLink} className="">Sailendra</a> </div>
+                <div className="">Developed with ❤ by <a href={footerLink} className="">Sailendra</a> </div>
                 <div className="copyright">Copyright &copy; <span id="year">0000</span> All rights reserved</div>
             </div>
         </footer>
